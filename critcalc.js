d30c327e7223ddfde34be5a56238d67cf941c55e
djmorosini <djmorosini@outlook.com>
2021-11-19 20:01:01 -0500

    functions

--- critcalc.js
+++ critcalc.js
@@ -3,26 +3,28 @@ function updateCrit() {
   //baseline
   crit = 50;
   // class
-  crit + 10;
-  //race
-  crit + 12;
-  //champion points
-  crit + 15;
-  crit + 10;
-  //mundus
-  crit + 17;
-  //group buffs
-  crit + 15;
-  crit + 20;
-  crit + 10;
-  crit + 10;
-  //personal loadout
-  crit + 6;
-  crit + 12;
-  crit + 10;
-  crit - 50;
-  crit + 2;
-  crit + 12;
+  for (let i = 1; i < 18; i++) {
+    var select = document.getElementById(i).value;
+    if(select == "Yes") {
+      if(i < 4 || i == 6 || i == 10 || i == 11 || i == 14) {
+        crit += 10;
+      } else if (i == 4 || i == 13 || i == 17) {
+        crit += 12;
+      } else if (i == 5 || i == 8) {
+        crit += 15;
+      } else if (i == 7) {
+        crit += 17;
+      } else if (i == 9) {
+        crit += 20;
+      } else if (i == 12) {
+        crit += 6;
+      } else if (i == 15) {
+        crit -= 50;
+      } else if (i == 16) {
+        crit += 2;
+      }
+    }
+  }
 
   document.getElementById("crit").innerHTML = crit + "%";
   document.getElementById("overcrit").innerHTML = (crit - 125) + "%";
